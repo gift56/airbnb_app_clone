9efa110fc31b4118386fb1aef0a34824a3630e03
Efe G <efeasiughu@gmail.com>
2024-04-29 10:44:29 -0400

    working on clerk api

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,9 +1,12 @@
-import { View, Text } from "react-native";
+import { useAuth } from "@clerk/clerk-expo";
+import { View, Text, Button } from "react-native";
 
 const Profile = () => {
+  const { signOut, isSignedIn } = useAuth();
+
   return (
     <View>
-      <Text>Profile</Text>
+      <Button title="Log Out" onPress={() => signOut()} />
     </View>
   );
 };

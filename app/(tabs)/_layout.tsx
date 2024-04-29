b36d49f62418dc52480c2eed46c7dd0cfe439a44
Efe G <efeasiughu@gmail.com>
2024-04-29 07:39:41 -0400

    added some material icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -58,7 +58,6 @@ const TabLayout = () => {
         name="profile"
         options={{
           tabBarLabel: "Profile",
-
           headerShown: false,
           tabBarIcon: ({ size, color }) => (
             <Ionicons name="person-circle-outline" size={size} color={color} />

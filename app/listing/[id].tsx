077f92845d69f91ab10d06918c6a030246194015
Efe G <efeasiughu@gmail.com>
2024-04-29 08:11:30 -0400

    just creating the file and folder structure

--- app/listing/[id].tsx
+++ app/listing/[id].tsx
@@ -1,6 +1,9 @@
+import { useLocalSearchParams } from "expo-router";
 import { View, Text } from "react-native";
 
 const Listingdetail = () => {
+  const { id } = useLocalSearchParams<{ id: string }>();
+
   return (
     <View>
       <Text>Listingdetail</Text>

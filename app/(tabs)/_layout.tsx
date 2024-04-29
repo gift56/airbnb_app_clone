c4614d4791b3727e9a0b2d47c0f83b757e3d7dd9
Efe G <efeasiughu@gmail.com>
2024-04-29 07:33:31 -0400

    added some material icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,8 @@
 import Colors from "@/constants/Colors";
 import { Tabs } from "expo-router";
+import { Ionicons } from '@expo/vector-icons';
+import { FontAwesome5 } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 const TabLayout = () => {
   return (

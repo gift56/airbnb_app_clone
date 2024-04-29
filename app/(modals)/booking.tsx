876fc7f92df6837cfb157c380dbc9ab404e7ad09
Efe G <efeasiughu@gmail.com>
2024-04-29 07:51:08 -0400

    done with down tab

---
+++ app/(modals)/booking.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const BookingModal = () => {
+  return (
+    <View>
+      <Text>BookingModal</Text>
+    </View>
+  );
+};
+
+export default BookingModal;

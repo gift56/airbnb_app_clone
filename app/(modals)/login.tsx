2e3e106be23c1f86282a03ec32d9a52165fdf27b
Efe G <efeasiughu@gmail.com>
2024-04-29 10:50:59 -0400

    working on clerk api

--- app/(modals)/login.tsx
+++ app/(modals)/login.tsx
@@ -1,6 +1,9 @@
-import { View, Text } from "react-native";
+import Colors from "@/constants/Colors";
+import { useWarmUpBrowser } from "@/hooks/useWarmUpBrowser";
+import { View, Text, StyleSheet } from "react-native";
 
 const LoginModal = () => {
+  useWarmUpBrowser();
   return (
     <View>
       <Text>LoginModal</Text>
@@ -9,3 +12,39 @@ const LoginModal = () => {
 };
 
 export default LoginModal;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    padding: 26,
+  },
+
+  seperatorView: {
+    flexDirection: 'row',
+    gap: 10,
+    alignItems: 'center',
+    marginVertical: 30,
+  },
+  seperator: {
+    fontFamily: 'mon-sb',
+    color: Colors.grey,
+    fontSize: 16,
+  },
+  btnOutline: {
+    backgroundColor: '#fff',
+    borderWidth: 1,
+    borderColor: Colors.grey,
+    height: 50,
+    borderRadius: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    paddingHorizontal: 10,
+  },
+  btnOutlineText: {
+    color: '#000',
+    fontSize: 16,
+    fontFamily: 'mon-sb',
+  },
+});

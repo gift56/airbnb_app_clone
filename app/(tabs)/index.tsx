c0a3dea8fdba5a16517d54712e77b27cd6d42308
Efe G <efeasiughu@gmail.com>
2024-04-29 18:31:12 -0400

    working on the homepage

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,20 @@
 import ExploreHeader from "@/components/ExploreHeader";
 import { Stack } from "expo-router";
+import { useState } from "react";
 import { View } from "react-native";
 
 const Homepage = () => {
+  const [category, setCategory] = useState<string>('Tiny homes');
+
+  const onDataChanged = (category: string) => {
+    setCategory(category);
+  };
+
   return (
     <View style={{ flex: 1 }}>
       <Stack.Screen
         options={{
-          header: () => <ExploreHeader onCategoryChanged={() => {}} />,
+          header: () => <ExploreHeader onCategoryChanged={onDataChanged} />,
         }}
       />
     </View>

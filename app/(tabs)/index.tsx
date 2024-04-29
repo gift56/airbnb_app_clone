d1555bb3853975c83fa34879ad80df483dbd3b27
Efe G <efeasiughu@gmail.com>
2024-04-29 18:21:17 -0400

    working on the homepage

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,17 @@
-import { Link } from "expo-router";
-import { View, Text } from "react-native";
+import ExploreHeader from "@/components/ExploreHeader";
+import { Stack } from "expo-router";
+import { View } from "react-native";
 
 const Homepage = () => {
-  return <View></View>;
+  return (
+    <View style={{ flex: 1 }}>
+      <Stack.Screen
+        options={{
+          header: () => <ExploreHeader onCategoryChanged={onDataChanged} />,
+        }}
+      />
+    </View>
+  );
 };
 
 export default Homepage;

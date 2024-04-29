08669cffff0336068a1a29aa2ccd0f4748a5bfeb
Efe G <efeasiughu@gmail.com>
2024-04-29 18:14:55 -0400

    working on the homepage

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,12 +2,7 @@ import { Link } from "expo-router";
 import { View, Text } from "react-native";
 
 const Homepage = () => {
-  return (
-    <View>
-      <Link href={"/(modals)/login"}>Login</Link>
-      <Link href={"/(modals)/booking"}>Booking</Link>
-    </View>
-  );
+  return <View></View>;
 };
 
 export default Homepage;
